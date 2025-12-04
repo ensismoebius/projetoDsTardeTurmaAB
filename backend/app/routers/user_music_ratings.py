b28954542392fc330e8a591aa55d5354b3b8ed0b
Felipe Cesare <felipegithub18@gmail.com>
2025-12-04 14:27:46 -0300

    função para criar novos ratings
    #486

--- backend/app/routers/user_music_ratings.py
+++ backend/app/routers/user_music_ratings.py
@@ -16,3 +16,16 @@ def get_rating_by_id(user_music_ratings_id: int):
         return response.data[0]
     return {"error": "rating not found"}
 
+@router.post("/user_music_ratings")
+def create_rating(user_music_ratings: User_music_ratings):
+    supabase = get_supabase()
+    new_rating = {
+       "rating": user_music_ratings.rating,
+        
+    }
+    response = supabase.table("user_music_ratings").insert(new_rating).execute()
+    if response.data:
+        return response.data[0]
+    return {"error": "Failed to create rating"} 
+
+
