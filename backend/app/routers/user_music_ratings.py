000ca90156dee80b330e3436bc9f51d0cd2f0a81
Felipe Cesare <felipegithub18@gmail.com>
2025-12-04 14:10:19 -0300

    função para chamar os ratings por id
    #486

--- backend/app/routers/user_music_ratings.py
+++ backend/app/routers/user_music_ratings.py
@@ -5,5 +5,13 @@ from app.db.supabase_client import get_supabase
 @router.get("/selectratings")
 def get_ratings():
     supabase = get_supabase()
-    response = supabase.table("ratings").select("*").execute()
+    response = supabase.table("rating").select("*").execute()
     return response.data  
+
+@router.get("/styles/{user_music_ratings_id}")
+def get_styles_by_id(User_music_ratings_id: int):
+    supabase = get_supabase()
+    response = supabase.table("styles").select("*").eq("id", User_music_ratings_id).execute()
+    if response.data:
+        return response.data[0]
+    return {"error": "style not found"}
